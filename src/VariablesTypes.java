198bfa0c3539f5aa85d332efcfb5879f4bfb0451
Vahe <_vahe_@mail.ru>
2021-03-27 22:34:51 +0400

    primitive types

---
+++ src/VariablesTypes.java
@@ -0,0 +1,32 @@
+public class VariablesTypes {
+    public static void main(String[] args) {
+//        whole numbers
+        byte v_byte = 6;
+        short v_short = 6435;
+        int v_int = 8674645;
+        long v_long = 634654654564L;
+
+//        fractional numbers
+        float v_float = 654.65F;
+        double v_double = 465546.56D;
+
+//        boolean
+        boolean v_boolean = true;
+
+//        char
+        char v_char = '\u6851';
+
+//        String
+        String v_string = "Hello world!";
+
+//        Print
+        System.out.println("byte = " + v_byte);
+        System.out.println("short = " + v_short);
+        System.out.println("int = " + v_int);
+        System.out.println("long = " + v_long);
+        System.out.println("float = " + v_float);
+        System.out.println("double = " + v_double);
+        System.out.println("char: " + v_char);
+        System.out.println("String: " + v_string);
+    }
+}
